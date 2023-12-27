f6088f4d743419af8c48afb25949b61190f2d98b
Paramkrishna <115072639+Paramkrishna@users.noreply.github.com>
2023-12-27 16:14:47 +0530

    Codes

--- Section 27 - Pytest - Advanced Testing Framework/test_command_line_args.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def test_command_line_methodA(oneTimeSetUp, setUp):
-    print("Running method A")
-
-def test_command_line_methodB(oneTimeSetUp, setUp):
-    print("Running method B")
--- Section 27 - Pytest - Advanced Testing Framework/test_conftest_demo1.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def test_demo1_methodA(oneTimeSetUp, setUp):
-    print("Running conftest demo1 method A")
-
-def test_demo1_methodB(oneTimeSetUp, setUp):
-    print("Running conftest demo1 method B")
